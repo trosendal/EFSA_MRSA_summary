27e4b81e9926e7e266859e9f2277a614bff492e2
Thomas Rosendal <thomas.rosendal@sva.se>
2025-12-12 19:58:00 +0100

    something like this for figure 3

--- R/figure3.R
+++ R/figure3.R
@@ -6,10 +6,23 @@
 ##' @return A table of figures
 figure3 <- function() {
     df <- read_AMR()
+    df$host <- host_association(df$SPA)
     setDT(df)
-    df[!is.na(CC_infer),
-       .N/19,
-       .(animalclass,
-         host_association,
-         CC_infer %in% c("398", "CC1/CC398"))]
+    df$samplingID <- as.numeric(
+        as.factor(
+            paste0(df$repYear,
+                   df$matrix_C,
+                   df$sampUnitType,
+                   df$repCountry,
+                   df$sampArea,
+                   df$totUnitsTested,
+                   df$totUnitsPositive)))
+    df <- df[, .(samplingID,
+                 animalclass,
+                 host_association,
+                 CC_infer = CC_infer %in% c("398", "CC1/CC398"),
+                 pos = totUnitsPositive)]
+    df <- df[, as.numeric(pos[1]),
+             by = .(samplingID, animalclass, host_association, CC_infer)]
+    df[, sum(V1), by = .(animalclass, host_association, CC_infer)]
 }
